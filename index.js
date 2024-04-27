c373c4639fb6c986c13e7367b52e6b6b533e69ce
Tyiss Goodman <sticktyiss@gmail.com>
2024-04-26 18:59:43 -0700

    Changed structure from dl to divs. Allows more flexibility and simplicity with the list and adding images.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const books = document.getElementById("books");
-
 
 const showList = (id) => {
   const menu = document.getElementById(id);
   const dlElement = menu.nextElementSibling;
   if (dlElement.style.display === "none") {
-    dlElement.style.display = "block"
+    dlElement.style.display = "flex"
   } else {
     dlElement.style.display = "none";
   }
