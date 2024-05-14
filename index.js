eac7bab28c0a4383d147c9eb2ea124ca76dc3004
Tyiss Goodman <sticktyiss@gmail.com>
2024-05-13 19:40:09 -0700

    Change the js to auto hide everything between menu switches.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 
 const showList = (id) => {
   const homeDiv = document.getElementById("home");
+  const booksDiv = document.getElementById("books");
+  const boardGamesDiv = document.getElementById("boardGames");
+  const comicsDiv = document.getElementById("comics");
+  const collectiblesDiv = document.getElementById("collectibles");
   homeDiv.style.display = "none";
+  booksDiv.style.display = "none";
+  boardGamesDiv.style.display = "none";
+  collectiblesDiv.style.display = "none";
+  comicsDiv.style.display = "none";
   const dlElement = document.getElementById(id);
   if (dlElement.style.display === "none") {
     dlElement.style.display = "flex"
